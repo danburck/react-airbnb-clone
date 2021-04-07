43b022a80827013a85c3eff54941f68fa6856c0c
danburck <daniel.m.burckhardt@gmail.com>
2021-04-07 23:53:56 +0200

    Feat (Flats): Add props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Flat from './components/flat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const flat = {
+      "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      "price": 164,
+      "priceCurrency": "EUR",
+      "lat": 48.884211,
+      "lng": 2.34689
+    };
+
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+          </div>
+        </div>
+        <div className="map"></div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
